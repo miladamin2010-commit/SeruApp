7f52925bd7c2cf1acc607cded1eba2287aab4b8f
Milad Amin <Milad.Amin@vanquisbank.co.uk>
2023-05-22 15:22:51 +0100

    data changes

--- src/database/data.js
+++ src/database/data.js
@@ -1,49 +1,105 @@
 export default [
     {
         id: 1,
-        question : "Javascript is an _______ language",
+        question : "Which of the following is NOT a requirement to obtain a license?",
         options : [
-            'Object-Oriented',
-            'Object-Based',
-            'Procedural',
+            'Being at least 21 years old when applying for the license',
+            'Having a driving license that is at least three years old',
+            'Having the right to live and work in the UK',
+            'Having a degree in English language and literature'
         ]
     },
     {
         id: 2,
-        question : "Following methods can be used to display data in some form using Javascript",
+        question : "What is one of the responsibilities of a licensed London PHV driver?",
         options : [
-            'document.write()',
-            'console.log()',
-            'window.alert()',
+            'Keeping their licence in their car at all times', 
+            'Ignoring any conditions attached to their licence' ,
+            'Keeping in contact with TfL and responding to communication', 
+            'Only working with unlicensed private hire operators'
         ]
     },
     {
         id: 3,
-        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
+        question : "What information is displayed on a PHV driver's badge?",
         options : [
-            'Boolean',
-            'Undefined',
-            'Object',
+            "The passenger's name and address",
+            "The driver's date of birth and phone number",
+            "The driver's name, photograph, licence number, and licence period end date",
+            "The driver's vehicle make and model"
         ]
     },
     {
         id: 4,
-        question : "What does the toString() method return?",
+        question : "What should you do if you lose your PHV driver's badge or licence?",
         options : [
-            'Return Object',
-            'Return String',
-            'Return Integer'
+            'Keep working as a PHV driver without it ',
+            'Report it immediately to TfL ',
+            'Wait for it to be found by someone and returned to you ',
+            'Ignore it and hope it will turn up later'
         ]
     },
     {
         id: 5,
-        question : "Which function is used to serialize an object into a JSON string?",
+        question : "What are the medical conditions that PHV drivers should inform TfL about?",
         options : [
-            'stringify()',
-            'parse()',
-            'convert()',
+            'Only diabetes and heart conditions ',
+            'Only poor eyesight and neurological conditions ',
+            'Only psychiatric illness and prescription medication ',
+            'Any condition that may affect their ability to drive, including diabetes, epilepsy, heart conditions, poor eyesight, neurological conditions, prescription medication, and psychiatric illness, among others.',
+        ]
+    },
+    {
+        id: 6,
+        question : "What should you do if the licence discs fixed to a PHV are damaged, lost, or stolen?",
+        options : [
+            'Keep using the vehicle for private hire purposes ',
+            'Wait for the licence discs to repair themselves ',
+            'Contact TfL immediately for replacement discs ',
+            'Try to remove the security features on the licence discs to reuse them',
+        ]
+    },
+    {
+        id: 7,
+        question : "Which of the following statements is true about PHV insurance?",
+        options : [
+            'PHV insurance is not required if the vehicle is only used for personal purposes. ',
+            'The insurance policy only needs to be valid when the vehicle is being used to transport passengers. ',
+            'Insurance details do not need to be carried by the driver while working. ',
+            'Insurance details must be provided to the police if requested, and to passengers if the vehicle is involved in a collision.',
+        ]
+    },
+    {
+        id: 8,
+        question : "What must you do if you own a PHV and it is involved in a collision?",
+        options : [
+            'Nothing, as long as the vehicle is still drivable. ',
+            'Report the collision to TfL only if it causes significant damage. ',
+            'Notify TfL within 24 hours of the collision, regardless of the extent of the damage. ',
+            'Inform TfL within 72 hours of the collision if it affects the safety, performance, appearance or comfort of the vehicle.',
+        ]
+    },
+    {
+        id: 9,
+        question : "What are the medical conditions that PHV drivers should inform TfL about?",
+        options : [
+            'Ignore the letter and continue to use the vehicle as normal. ',
+            'Follow the instructions in the letter regarding any necessary repairs. ',
+            'Disregard the letter if the vehicle is still drivable and the issue is not affecting its performance. ',
+            'Only follow the instructions in the letter if the vehicle is not currently being used for private hire purposes.',
+        ]
+    },
+    {
+        id: 10,
+        question : "What should you do if TfL or the vehicle manufacturer sends you a letter regarding your PHV??",
+        options : [
+            'Yes, as long as they approach you and ask for a ride. ',
+            'No, unless they are willing to pay a higher fare. ',
+            'No, unless they have booked you through a licensed operator. ',
+            'Only if you are parked and waiting for a booking.',
         ]
     }
+
 ];
 
-export const answers = [0, 1, 2, 1, 0];
+export const answers = [4, 3, 3, 2, 4, 3, 4, 4, 2, 3 ];
